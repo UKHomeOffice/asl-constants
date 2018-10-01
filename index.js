cf65d6b6d3791a3def976884ea2fd5e26a1f5d15
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-10-01 17:53:11 +0100

    fix typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const externalPermissions = require('./constants/external-permissions');
 const holdingCodes = require('./constants/holding-codes');
 const moduleCodes = require('./constants/module-codes');
 const pilStatuses = require('./constants/pil-statuses');
-const proceedureCodes = require('./constants/proceedure-codes');
+const procedureCodes = require('./constants/procedure-codes');
 const projectStatuses = require('./constants/project-statuses');
 const roles = require('./constants/roles');
 const suitabilityCodes = require('./constants/suitability-codes');
@@ -20,7 +20,7 @@ module.exports = {
   holdingCodes,
   moduleCodes,
   pilStatuses,
-  proceedureCodes,
+  procedureCodes,
   projectStatuses,
   roles,
   suitabilityCodes
